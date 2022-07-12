d19603c56e81044558177d9ad0be253477efa467
ADKcode <790532173@qq.com>
2022-07-12 13:34:16 +0800

    feat: 🎸 更新 flutter 的表单

---
+++ flutter-first/myfirstapp/lib/main-routes.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+/// 案例1：简单的无限列表示例
+void main(List<String> args) {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '我的app',
+      initialRoute: '/',
+      routes: {
+        "/search": (context) => const MySearchPage(
+              title: '这是路由',
+            ),
+      },
+      home: HomePage(),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('首页'),
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        height: double.infinity,
+        color: Colors.blue,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('首页'),
+            ElevatedButton(
+              child: const Text('跳转到第二页'),
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return MySearchPage(title: '这是搜索');
+                    }, //括号里可以传参
+                  ),
+                );
+              },
+            ),
+            ElevatedButton(
+              child: const Text('跳转到第二页'),
+              onPressed: () {
+                Navigator.pushNamed(context, '/search', arguments: '你好');
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MySearchPage extends StatelessWidget {
+  final String title;
+  const MySearchPage({Key? key, required this.title}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('搜索页'),
+      ),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: Colors.red,
+        child: Text(
+            '搜索页$title 携带的参数是${ModalRoute.of(context)?.settings.arguments}'),
+      ),
+    );
+  }
+}
